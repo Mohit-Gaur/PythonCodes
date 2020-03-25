be2ff7960a087184d1aefe51b4b847f30375a38b
Mohit Gaur <56885276+Mohit-Gaur@users.noreply.github.com>
2020-03-25 18:21:38 +0530

    Create gradebook.py
    Created a gradebook

---
+++ gradebook.py
@@ -0,0 +1,11 @@
+subjects = ['physics', 'calculus', 'poetry', 'history']
+grades = [98, 97, 85, 88]
+subjects.append('computer science')
+grades.append(100)
+gradebook = list(zip(grades, subjects))
+gradebook.append('visual arts')
+gradebook.append(93)
+print(gradebook)
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+full_gradebook = gradebook + last_semester_gradebook
+print(full_gradebook)
